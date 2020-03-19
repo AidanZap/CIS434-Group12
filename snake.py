3d7ea5c8cad249d32c7b36c6fe2997c2a17b39bc
dowoods97 <d.o.woods97@gmail.com>
2020-03-19 13:02:36 -0400

    working base game V1.0

--- snake.py
+++ snake.py
@@ -1,83 +1,208 @@
+# Playlist I was looking at : https://www.youtube.com/watch?v=i6xMBig-pP4
+
+
+import math
+import random
 import pygame
+import tkinter as tk
+from tkinter import messagebox
 
-GRID_SQUARE = 50
 
 
-class App:
 
-    def __init__(self):
-        pygame.init()
-        self.window_width = 12 * GRID_SQUARE
-        self.window_height = 16 * GRID_SQUARE
-        self.win = pygame.display.set_mode((self.window_width, self.window_height))
-        pygame.display.set_caption("PythonPythonGame")
-        self.time_delay = 100
-        self.snake = Snake()
-        self.running = True
+class cube(object):
+    rows = 20
+    w = 500
+    def __init__(self, start, dirnx = 1, dirny = 0, color = (255,0,0)):
+        self.pos = start
+        self.dirnx = 1
+        self.dirny = 0
+        self.color = color
 
-    def start_game(self):
-        while self.running:
-            pygame.time.delay(self.time_delay)
+    def move(self, dirnx, dirny):
+        self.dirnx = dirnx
+        self.dirny = dirny
+        self.pos = (self.pos[0] + self.dirnx, self.pos[1] + self.dirny)
 
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.running = False
+    def draw(self, surface, eyes=False):
+        dis = self.w // self.rows
+        i = self.pos[0]
+        j = self.pos[1]
 
-            # ***** Player input ***** #
+        pygame.draw.rect(surface, self.color, (i*dis + 1,j*dis + 1, dis-2, dis-2))
+
+        if eyes:
+            center = dis//2
+            radius = 3
+            circleMiddle = (i*dis + center-radius, j* dis + 8)
+            circleMiddle2 = (i*dis + dis-radius * 2, j* dis + 8)
+            pygame.draw.circle(surface, (0,0,0), circleMiddle, radius)
+            pygame.draw.circle(surface, (0,0,0), circleMiddle2, radius)
+
+class snake(object):
+    body = []
+    turns = {}
+    def __init__(self, color, pos):
+        self.color = color
+        self.head = cube(pos)
+        self.body.append(self.head)
+        self.dirnx = 0
+        self.dirny = 1
+
+    def move(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
             keys = pygame.key.get_pressed()
-            if keys[pygame.K_LEFT]:
-                self.snake.face_left()
-            if keys[pygame.K_RIGHT]:
-                self.snake.face_right()
-            if keys[pygame.K_UP]:
-                self.snake.face_up()
-            if keys[pygame.K_DOWN]:
-                self.snake.face_down()
 
-            self.snake.update()
+            for key in keys:
+                if keys[pygame.K_LEFT]:
+                    self.dirnx = -1
+                    self.dirny = 0
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
 
-            app.win.fill((0, 0, 0))
-            pygame.draw.rect(app.win, (255, 0, 0), (self.snake.x, self.snake.y, self.snake.width, self.snake.height))
-            pygame.display.update()
+                elif keys[pygame.K_RIGHT]:
+                    self.dirnx = 1
+                    self.dirny = 0
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
 
+                elif keys[pygame.K_UP]:
+                    self.dirny = -1
+                    self.dirnx = 0
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
 
-class Snake:
-
-    def __init__(self):
-        self.x = 6 * GRID_SQUARE
-        self.y = 8 * GRID_SQUARE
-        self.width = GRID_SQUARE
-        self.height = GRID_SQUARE
-        self.vel = GRID_SQUARE
-        self.facing = "RIGHT"
-
-    def face_left(self):
-        if self.facing is not "RIGHT":
-            self.facing = "LEFT"
-
-    def face_right(self):
-        if self.facing is not "LEFT":
-            self.facing = "RIGHT"
-
-    def face_up(self):
-        if self.facing is not "DOWN":
-            self.facing = "UP"
-
-    def face_down(self):
-        if self.facing is not "UP":
-            self.facing = "DOWN"
-
-    def update(self):
-        if self.facing == "UP":
-            self.y -= self.vel
-        elif self.facing == "DOWN":
-            self.y += self.vel
-        elif self.facing == "LEFT":
-            self.x -= self.vel
-        elif self.facing == "RIGHT":
-            self.x += self.vel
+                elif keys[pygame.K_DOWN]:
+                    self.dirny = 1
+                    self.dirnx = 0
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
 
 
-# ***** Start ***** #
-app = App()
-app.start_game()
+        for i, c in enumerate(self.body):
+            p = c.pos[:]
+            if p in self.turns:
+                turn = self.turns[p]
+                c.move(turn[0], turn[1])
+                if i == len(self.body) - 1:
+                    self.turns.pop(p)
+            else:
+                if c.dirnx == -1 and c.pos[0] <= 0: c.pos = (c.rows-1, c.pos[1])
+                elif c.dirnx == 1 and c.pos[0] >= c.rows-1: c.pos = (0,c.pos[1])
+                elif c.dirny == 1 and c.pos[1] >= c.rows -1: c.pos = (c.pos[0], 0)
+                elif c.dirny == -1 and c.pos[1] <=0: c.pos = (c.pos[0], c.rows -1)
+                else: c.move(c.dirnx, c.dirny)
+
+    
+    def reset(self, pos):
+        self.head = cube(pos)
+        self.body = []
+        self.body.append(self.head)
+        self.turns = {}
+        self.dirnx = 0
+        self.dirny = 1
+
+    def addCube(self):
+        tail = self.body[-1]
+        dx, dy = tail.dirnx, tail.dirny
+
+        if dx == 1 and dy == 0:
+            self.body.append(cube((tail.pos[0]-1, tail.pos[1])))
+        elif dx == -1 and dy == 0:
+            self.body.append(cube((tail.pos[0]+1, tail.pos[1])))
+        elif dx == 0 and dy == 1:
+            self.body.append(cube((tail.pos[0], tail.pos[1]-1)))
+        elif dx == 0 and dy == -1:
+            self.body.append(cube((tail.pos[0], tail.pos[1]+1)))
+
+        self.body[-1].dirnx = dx
+        self.body[-1].dirny = dy
+
+
+    def draw(self, surface):
+        for i, c in enumerate(self.body):
+            if i == 0:
+                c.draw(surface, True)
+            else:
+                c.draw(surface)
+
+def drawGrid(w, rows, surface):
+    rowWidth = width // rows
+    x = 0
+    y = 0
+
+    for i in range(rows):
+        x += rowWidth
+        y += rowWidth
+        pygame.draw.line(surface, (150,150,150), (x,0), (x,w))
+        pygame.draw.line(surface, (150,150,150), (0,y), (w,y))
+
+def redrawWindow(surface):
+    global rows, width, s, snack
+    surface.fill((0,0,0))
+    s.draw(surface)
+    snack.draw(surface)
+    drawGrid(width, rows, surface)
+    pygame.display.update()
+
+def randomSnack(rows, item):
+
+    positions = item.body
+
+    while True:
+        x = random.randrange(rows)
+        y = random.randrange(rows)
+
+        if len(list(filter(lambda z:z.pos == (x,y), positions)))> 0:
+             continue
+        else: 
+            break
+
+    return (x,y)
+
+def message_box(subject,content):
+    root = tk.Tk()
+    root.attributes("-topmost", True)
+    root.withdraw()
+    messagebox.showinfo(subject,content)
+    try:
+        root.destroy()
+    except:
+        pass
+
+
+
+def main():
+
+    global width, rows, s, snack
+    width = 500
+    rows = 20
+    win = pygame.display.set_mode((width, width))
+    s = snake((255,0,0), (10,10))
+    snack = cube(randomSnack(rows, s), color=(0,255,0))
+    flag = True
+
+    clock = pygame.time.Clock()
+
+    while flag:
+        pygame.time.delay(50)
+        clock.tick(10)
+        s.move()
+        if s.body[0].pos == snack.pos:
+            s.addCube()
+            snack = cube(randomSnack(rows, s), color=(0,255,0))
+
+        for x in range(len(s.body)):
+            if s.body[x].pos in list(map(lambda z:z.pos,s.body[x+1:])):
+                print('Score: ', len(s.body))
+                message_box('You Lost!','Play Again..')
+                s.reset((10,10))
+                break
+
+
+
+        redrawWindow(win)
+        
+    pass
+
+
+main()
