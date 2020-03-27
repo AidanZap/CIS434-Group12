49aba257b44b706f4e65e8ceae03339d7f9cd26b
aidan <aidanzduck@gmail.com>
2020-03-27 17:18:13 -0400

    Working menu!

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Playlist I was looking at : https://www.youtube.com/watch?v=i6xMBig-pP4
 import random
 import pygame
-import pygame.locals
+from pygame.locals import *
 import cube
 import snake
 
@@ -41,53 +41,98 @@ def random_snack(rows, item):
     return x, y
 
 
-def menu(surface, clock):
-    on_menu = True
-    pygame.font.init()
-    font = pygame.font.SysFont("Arial", 72)
-    text_obj = font.render("PythonPythonGame", 1, (0, 255, 0))
+def draw_text(text, font, text_color, surface, x, y):
+    text_obj = font.render(text, 1, text_color)
     text_rect = text_obj.get_rect()
-    text_rect.topleft = (200, 200)
+    text_rect.topleft = (x, y)
     surface.blit(text_obj, text_rect)
-
-    while on_menu:
-        for event in pygame.event.get():
-            if event.type == pygame.quit():
-                pygame.quit()
-
-        pygame.display.update()
-        clock.tick(60)
 
 
 def main():
+    pygame.init()
     width = 500
     rows = 20
     surface = pygame.display.set_mode((width, width))
-    pygame.display.update()
+    pygame.display.set_caption("PythonPythonGame")
+    clock = pygame.time.Clock()
+    font = pygame.font.SysFont("Arial", 32)
 
     # ***** Game Objects ***** #
-    clock = pygame.time.Clock()
     s = snake.snake((255, 0, 0), (10, 10))
     snack = cube.cube(random_snack(rows, s), color=(0, 255, 0))
 
-    # menu(surface, clock)
+    # ***** Main Loop ***** #
+    on_menu = True
+    playing = False
+    main_loop = True
 
-    playing = True
-    while playing:
-        pygame.time.delay(50)
-        clock.tick(10)
-        s.move()
-        if s.body[0].pos == snack.pos:
-            s.addCube()
-            snack = cube.cube(random_snack(rows, s), color=(0, 255, 0))
+    while main_loop:
 
-        for x in range(len(s.body)):
-            if s.body[x].pos in list(map(lambda z: z.pos, s.body[x+1:])):
-                print('Score: ', len(s.body))
-                s.reset((10, 10))
-                break
+        while on_menu:
+            surface.fill((0, 0, 0))
 
-        redraw_window(surface, s, snack, width, rows)
+            start_button = pygame.Rect(160, 210, 180, 60)
+            pygame.draw.rect(surface, (0, 128, 0), start_button)
+            draw_text("Start Game", font, (255, 255, 255), surface, 170, 220)
+
+            two_player_button = pygame.Rect(140, 280, 230, 60)
+            pygame.draw.rect(surface, (0, 128, 0), two_player_button)
+            draw_text("2-Player Game", font, (255, 255, 255), surface, 150, 290)
+
+            settings_button = pygame.Rect(140, 350, 230, 60)
+            pygame.draw.rect(surface, (0, 128, 0), settings_button)
+            draw_text("Game Settings", font, (255, 255, 255), surface, 150, 360)
+
+            quit_button = pygame.Rect(212, 420, 80, 60)
+            pygame.draw.rect(surface, (0, 128, 0), quit_button)
+            draw_text("Quit", font, (255, 255, 255), surface, 220, 430)
+
+            mx, my = pygame.mouse.get_pos()
+            if start_button.collidepoint(mx, my):
+                if click:
+                    on_menu = False
+                    playing = True
+            if two_player_button.collidepoint((mx, my)):
+                if click:
+                    pass
+            if settings_button.collidepoint(mx, my):
+                if click:
+                    pass
+            if quit_button.collidepoint(mx, my):
+                if click:
+                    on_menu = False
+                    main_loop = False
+
+            click = False
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    quit()
+                if event.type == MOUSEBUTTONDOWN:
+                    click = True
+
+            pygame.display.update()
+            clock.tick(60)
+
+        while playing:
+            pygame.time.delay(50)
+            clock.tick(10)
+            s.move()
+            if s.body[0].pos == snack.pos:
+                s.addCube()
+                snack = cube.cube(random_snack(rows, s), color=(0, 255, 0))
+
+            for x in range(len(s.body)):
+                if s.body[x].pos in list(map(lambda z: z.pos, s.body[x+1:])):
+                    print('Score: ', len(s.body))
+                    playing = False
+                    on_menu = True
+                    break
+
+            redraw_window(surface, s, snack, width, rows)
+
+    pygame.quit()
+    quit()
 
 
 if __name__ == "__main__":
